f429819db09cb13722414212a3c584607fe504b0
Adrish123 <adrish.au94@gmail.com>
2021-11-19 13:40:54 +0530

    Test file

---
+++ .mvn/wrapper/module-info.java
@@ -0,0 +1,7 @@
+module SpringRest {
+	exports com.springrest.springrest.controller;
+	exports com.springrest.springrest;
+	exports com.springrest.springrest.dao;
+	exports com.springrest.springrest.entities;
+	exports com.springrest.springrest.services;
+}
