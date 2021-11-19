99d02466793c676a4726cddcd96d11d5340a3957
Adrish123 <adrish.au94@gmail.com>
2021-11-19 13:46:35 +0530

    Test file 2

--- src/main/java/com/springrest/springrest/SpringrestApplication.java
+++ src/main/java/com/springrest/springrest/SpringrestApplication.java
@@ -9,6 +9,7 @@ public class SpringrestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringrestApplication.class, args);
 		System.out.println("Test");
+		System.out.println("test2");
 	}
 
 }
